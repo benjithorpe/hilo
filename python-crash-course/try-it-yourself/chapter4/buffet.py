c5efc4aea3960c44a6adb86b9e4783a7c6ee38ca
benjithorpe <benjaminthorpe19@gmail.com>
2021-04-29 00:23:38 +0000

    Covered tuples and their use cases

---
+++ python-crash-course/try-it-yourself/chapter4/buffet.py
@@ -0,0 +1,11 @@
+foods = ("Rice", "Salad", "Pizza", "Hamburger", "French Fries")
+
+for food in foods:
+    print(food)
+
+
+print("\nNew Food Menu")
+foods = ("Bacon", "Salad", "Pizza", "Hamburger", "Macaroni")
+
+for food in foods:
+    print(food)
